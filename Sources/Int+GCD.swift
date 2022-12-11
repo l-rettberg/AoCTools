a36abb19cc2e98c9a54115e7640ace02c15c7179
Gereon Steffens <gereon@steffens.org>
2022-12-11 12:59:26 +0100

    make gcd/lcm public

--- Sources/Int+GCD.swift
+++ Sources/Int+GCD.swift
@@ -9,7 +9,7 @@
 ///   - m: Int
 ///   - n: Int
 /// - Returns: the greatest common divisor of `m` and `n`
-func gcd(_ m: Int, _ n: Int) -> Int {
+public func gcd(_ m: Int, _ n: Int) -> Int {
     let r = m % n
     if r != 0 {
         return gcd(n, r)
@@ -23,6 +23,6 @@ func gcd(_ m: Int, _ n: Int) -> Int {
 ///   - m: Int
 ///   - n: Int
 /// - Returns: the lowest common multiple of `m` and `n`
-func lcm(_ m: Int, _ n: Int) -> Int {
+public func lcm(_ m: Int, _ n: Int) -> Int {
     m / gcd(m, n) * n
 }
---
+++ Tests/GCDTests.swift
@@ -0,0 +1,28 @@
+//
+//  GCDTests.swift
+//  
+//
+//  Created by Gereon Steffens on 11.12.22.
+//
+
+import XCTest
+import AoCTools
+
+final class GCDTests: XCTestCase {
+
+    func testGCD() throws {
+        XCTAssertEqual(gcd(10, 9), 1)
+        XCTAssertEqual(gcd(9, 6), 3)
+        XCTAssertEqual(gcd(12, 6), 6)
+        XCTAssertEqual(gcd(16, 8), 8)
+        XCTAssertEqual(gcd(10, 8), 2)
+    }
+
+    func testLCM() throws {
+        XCTAssertEqual(lcm(10, 9), 90)
+        XCTAssertEqual(lcm(10, 2), 10)
+        XCTAssertEqual(lcm(12, 3), 12)
+        XCTAssertEqual(lcm(5, 7), 35)
+        XCTAssertEqual(lcm(12, 8), 24)
+    }
+}
